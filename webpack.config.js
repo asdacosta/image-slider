e664a3310ec63e59950d6fc9374ec1a989338d1b
asdacosta <asdacosta18@gmail.com>
2023-11-29 19:34:30 +0000

    Add configuration for videos

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "imgs/[name][ext]",
+          filename: 'imgs/[name][ext]',
+        },
+      },
+      {
+        test: /\.(mp4|webm)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'imgs/[name][ext]',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      inject: "head",
-      scriptLoading: "defer",
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'head',
+      scriptLoading: 'defer',
     }),
   ],
 };
