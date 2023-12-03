64f4fb3f635cfe2cb2de6fe14b9f08012bc710b7
asdacosta <asdacosta18@gmail.com>
2023-12-03 00:02:59 +0000

    Comment out development config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // mode: 'development',
+  // devtool: 'inline-source-map',
   mode: 'production',
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -39,4 +42,7 @@ module.exports = {
       scriptLoading: 'defer',
     }),
   ],
+  // devServer: {
+  //   static: './src',
+  // },
 };
